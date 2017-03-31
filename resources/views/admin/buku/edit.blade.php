db0039773e9ba017b3888f61720c905d6ccfc31c
HADI-PC\acer <hadisaputrami@gmail.com>
2017-03-31 13:30:31 +0800

    Revert "Assset & Include"
    This reverts commit f016ea51d70a785b5834dd264acdf16e96afd356.

--- resources/views/admin/buku/edit.blade.php
+++ resources/views/admin/buku/edit.blade.php
@@ -1,8 +1,10 @@
-@extends('admin.layout')
+@extends('layouts.app')
 
 @section('content')
     <div class="container">
         <div class="row">
+            @include('admin.sidebar')
+
             <div class="col-md-9">
                 <div class="panel panel-default">
                     <div class="panel-heading">Edit buku #{{ $buku->id }}</div>
--- resources/views/admin/include/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-        <b>Version</b> 2.3.7
-    </div>
-    <strong>Copyright 2017 <a href="http://almsaeedstudio.com">IT MI</a>.</strong> All rights
-    reserved.
-</footer>
