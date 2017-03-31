f016ea51d70a785b5834dd264acdf16e96afd356
HADI-PC\acer <hadisaputrami@gmail.com>
2017-03-31 13:10:52 +0800

    Assset & Include

--- resources/views/admin/buku/create.blade.php
+++ resources/views/admin/buku/create.blade.php
@@ -1,10 +1,8 @@
-@extends('layouts.app')
+@extends('admin.layout')
 
 @section('content')
     <div class="container">
         <div class="row">
-            @include('admin.sidebar')
-
             <div class="col-md-9">
                 <div class="panel panel-default">
                     <div class="panel-heading">Create New buku</div>
