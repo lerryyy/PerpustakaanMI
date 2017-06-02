b240fa8ccb79c8ebd9846d34d29fcf34d9ae16e5
EKO <eko_here@yahoo.co.id>
2017-06-02 10:28:34 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('/test-oauth2',function(){
 	return 'sukses';
 })->middleware('auth:api');
+
+Route::get('/cari_buku','BukuController@cariBuku');
