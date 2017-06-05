a273fd04bba19527eade26fcf0778b6c50fb4f33
MR-HAN <johandx1996@gmail.com>
2017-06-05 02:34:37 -0700

    Name User Login

--- resources/views/admin/include/header.blade.php
+++ resources/views/admin/include/header.blade.php
@@ -31,8 +31,8 @@
                                 <img src="{{asset('assets/admin/images/!logged-user.jpg')}}" alt="Joseph Doe" class="img-circle" data-lock-picture="assets/images/!logged-user.jpg" />
                             </figure>
                             <div class="profile-info" data-lock-name="John Doe" data-lock-email="johndoe@okler.com">
-                                <span class="name">John Doe Junior</span>
-                                <span class="role">administrator</span>
+                                <span class="name">{{isset(Auth::user()->name)?Auth::user()->name:''}}</span>
+                                <span class="role">Admin Perpustakaan</span>
                             </div>
             
                             <i class="fa custom-caret"></i>
